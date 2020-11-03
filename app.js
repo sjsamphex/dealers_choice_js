ad0a09e38646b42bf6f7ea0e0052696aebf9c8c6
sjsamphex <sjsamphex@gmail.com>
2020-11-03 09:53:00 -0800

    implement html template tags

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const bookBank = require('./bookBank.js');
+var html = require('html-template-tag');
 
 express.static('./');
 app.use(express.static('public'));
@@ -10,41 +11,40 @@ app.use(express.static('public/images'));
 app.use(morgan('dev'));
 app.get('/', (req, res, next) => {
   const books = bookBank.list();
-  const html = `<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Cat Books</title>
-    <link rel="stylesheet" href="/main.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
-    <div class="book-list">
-      <header><img src="/CatBookLogo.png"/> <a href="/">Cat Books </a><img src="/CatBookLogo.png"/></header>
-      ${books
-        .map(
-          (book) => `
-        <div class='book-item'>
-          <div class='book-image'>
-            <img src="/${book.image}"/>
-          </div>
-          <div class='book-info'>
-            <p>
-              <span class="news-position"><a href="/books/${book.id}">${book.title}</a>. </span>
-              <br>
-              <small>(by ${book.author})</small>
-            </p>
-            <small>
-              Rating: ${book.rating} ⭑
-            </small>
-          </div>
-
-        </div>`
-        )
-        .join('')}
-    </div>
-  </body>
-</html>`;
-  res.send(html);
+  let htmlscript = html`<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Cat Books</title>
+        <link rel="stylesheet" href="/main.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div class="book-list">
+          <header>
+            <img src="/CatBookLogo.png" /> <a href="/">Cat Books </a
+            ><img src="/CatBookLogo.png" />
+          </header>
+          ${books.map(
+            (book) => html`<div class="book-item">
+              <div class="book-image">
+                <img src="/${book.image}" />
+              </div>
+              <div class="book-info">
+                <p>
+                  <span class="news-position"
+                    ><a href="/books/${book.id}">${book.title}</a>.
+                  </span>
+                  <br />
+                  <small>(by ${book.author})</small>
+                </p>
+                <small> Rating: ${book.rating} ⭑ </small>
+              </div>
+            </div>`
+          )}
+        </div>
+      </body>
+    </html>`;
+  res.send(htmlscript);
 });
 
 app.get('/books/:id', (req, res, next) => {
@@ -53,56 +53,62 @@ app.get('/books/:id', (req, res, next) => {
   if (!book.id) {
     next(new Error('Not Found'));
   }
-  res.send(`<!DOCTYPE html>
-  <html>
-  <head>
-    <title>Cat Books</title>
-    <link rel="stylesheet" href="/book.css" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  </head>
-  <body>
-    <div class="book-list">
-      <header><img src="/CatBookLogo.png"/> <a href="/">Cat Books </a><img src="/CatBookLogo.png"/></header>
-       <div class='book-item'>
-          <div class='book-image'>
-            <img src="/${book.image}"/>
-          </div>
-          <div class ='book-info'>
-            <p>
-              ${book.title}
-              <br>
-              <small>(by ${book.author})</small>
-            </p>
-            <small>
-              Rating: ${book.rating} ⭑
-            </small>
-            <p>${book.content}</p>
+  res.send(html`<!DOCTYPE html>
+    <html>
+      <head>
+        <title>Cat Books</title>
+        <link rel="stylesheet" href="/book.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div class="book-list">
+          <header>
+            <img src="/CatBookLogo.png" /> <a href="/">Cat Books </a
+            ><img src="/CatBookLogo.png" />
+          </header>
+          <div class="book-item">
+            <div class="book-image">
+              <img src="/${book.image}" />
+            </div>
+            <div class="book-info">
+              <p>
+                ${book.title}
+                <br />
+                <small>(by ${book.author})</small>
+              </p>
+              <small> Rating: ${book.rating} ⭑ </small>
+              <p>${book.content}</p>
+            </div>
           </div>
         </div>
-    </div>
-  </body>
-</html>`);
+      </body>
+    </html>`);
 });
 
 app.use(function (err, req, res, next) {
   console.error(err.stack);
-  const html = `<!DOCTYPE html>
+  let htmlscript = html`<!DOCTYPE html>
     <html>
-    <head>
-      <title>Cat Books</title>
-      <link rel="stylesheet" href="/book.css" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-      <div class="book-list">
-        <header><img src="/CatBookLogo.png"/> <a href="/">Cat Books </a><img src="/CatBookLogo.png"/></header>
-        <br>
-         <img src="/sadcat.jpg" style="width:auto;height:auto;">
-         <p style="text-align:center">This page doesn't exist. Please go back :(</p>
-      </div>
-    </body>
-  </html>`;
-  res.status(404).send(html);
+      <head>
+        <title>Cat Books</title>
+        <link rel="stylesheet" href="/book.css" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </head>
+      <body>
+        <div class="book-list">
+          <header>
+            <img src="/CatBookLogo.png" /> <a href="/">Cat Books </a
+            ><img src="/CatBookLogo.png" />
+          </header>
+          <br />
+          <img src="/sadcat.jpg" style="width:auto;height:auto;" />
+          <p style="text-align:center">
+            This page doesn't exist. Please go back :(
+          </p>
+        </div>
+      </body>
+    </html>`;
+  res.status(404).send(htmlscript);
 });
 
 const PORT = 3000;
